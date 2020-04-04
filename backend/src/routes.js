00ff2fba215b9f17bdd8d68af48055cda0b73380
PETsy <giulianolourencon@gmail.com>
2020-04-04 18:49:29 -0300

    create and list ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,12 +1,28 @@
 const express = require("express");
+const crypto = require("crypto");
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.get("/", (req, res) => {
-    return res.json({
-        evento: "aaaa",
-        aluno: "eu"
+routes.get('/ongs', async (req, res) => {
+    const ongs = await connection('ongs').select('*');
+    return res.json(ongs);
+});
+
+routes.post("/ongs", async (req, res) => {
+    const { name, email, wpp, city, uf } = req.body;
+
+    const id = crypto.randomBytes(4).toString("HEX");
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        wpp,
+        city,
+        uf
     });
+
+    return res.json({ id });
 });
 
 module.exports = routes;
