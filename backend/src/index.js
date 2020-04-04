2fc03d391e21fc3ad4e8855900dd6bd990f1298c
PETsy <giulianolourencon@gmail.com>
2020-04-04 18:20:28 -0300

    database config

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,11 @@
 const express = require("express");
+const routes = require("./routes");
+
 const app = express();
 
 app.use(express.json());
+app.use(routes);
 
-app.get("/", (req, res) => {
-    return res.json({
-        evento: "aaaa",
-        aluno: "eu"
-    });
-});
 
 app.listen(3333, () => {
     console.log("Server is Running on Port 3333!");
